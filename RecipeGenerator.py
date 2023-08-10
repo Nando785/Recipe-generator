00c1a3f39846b114f3d1b26e8c56301063709f6d
Fernando Rodriguez Martinez <Fernandormartinez21@gmail.com>
2023-08-09 21:18:02 -0700

    Started implementing API calling
    Setup calls to API, need to parse information and get search information from user input

--- RecipeGenerator.py
+++ RecipeGenerator.py
@@ -1,1 +1,47 @@
+import requests
 
+#======================================== RECIPE FINDER ========================================
+
+#parameters customized through "ingredients_list"
+ingredients = "apples,flour,sugar"
+numberOfRecipes = 2
+ignorePantry = True
+ranking = 2
+
+url = "https://api.spoonacular.com/recipes/findByIngredients"
+
+querystring = {"ingredients":ingredients, 
+               "number":numberOfRecipes,
+               "ignorePantry":ignorePantry,
+               "ranking":ranking}
+
+headers = {
+    "x-api-key" : "170112d56069460d9a732b82cc328a8e",
+    "X-RapidAPI-Host" : "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
+}
+
+response = requests.get(url, headers=headers, params=querystring)
+
+print(response.status_code)
+#print(response.json())
+for recipe in response:
+    print("Recpie name:" + recipe['title'])
+
+
+#===================================== RECIPE INFO FINDER =====================================
+
+#ids for all resulting recipes will be parsed from the first api call and used in
+#the new api call for recipe info
+id = 673463
+
+infoURL = "https://api.spoonacular.com/recipes/{id}/information"
+
+#===== headers already declared above =====
+# headers = {
+#     "x-api-key" : "170112d56069460d9a732b82cc328a8e",
+#     "X-RapidAPI-Host" : "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
+# }
+
+#response = requests.get(infoURL, headers=headers)
+
+print(response.json())
