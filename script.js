55b49af72b967ff46f8d431ab5c4aa8ecd84ffc5
Fernando Rodriguez Martinez <Fernandormartinez21@gmail.com>
2023-08-11 20:31:24 -0700

    Added checklist button response

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+const ingredients = document.querySelectorAll('.ingredient');
 
+ingredients.forEach(ingredient => {
+    ingredient.addEventListener('click', function () {
+        const isChecked = this.getAttribute('data-checked') === 'true';
+        this.setAttribute('data-checked', !isChecked);
+        this.classList.toggle('checked');
+    });
+});
