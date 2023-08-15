26f9670ba1292173da7b2d3547ffbeabbb17a896
Fernando Rodriguez Martinez <Fernandormartinez21@gmail.com>
2023-08-14 20:46:33 -0700

    Fixed submit button coloring issue

--- static/script.js
+++ static/script.js
@@ -1,4 +1,5 @@
 const ingredients = document.querySelectorAll('.ingredient');
+const submitButton = document.querySelector('.submit_button');
 
 ingredients.forEach(ingredient => {
     ingredient.addEventListener('click', function () {
@@ -6,4 +7,11 @@ ingredients.forEach(ingredient => {
         this.setAttribute('data-checked', !isChecked);
         this.classList.toggle('checked');
     });
+});
+
+submitButton.addEventListener('click', function () {
+    const isChecked = this.getAttribute('data-checked') === 'true';
+    this.setAttribute('data-checked', !isChecked);
+    // this.classList.toggle('submit_button.checked');
+    submitButton.classList.toggle('checked');
 });
